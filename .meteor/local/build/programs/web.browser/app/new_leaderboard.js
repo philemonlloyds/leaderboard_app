fd079fe9d14163e219a3d5d40942ba622099fc48
Philemon <philemon@Allens-MacBook-Pro.local>
2015-09-08 20:06:49 -0700

    Create mongodb collection and insert data in the js file

--- .meteor/local/build/programs/web.browser/app/new_leaderboard.js
+++ .meteor/local/build/programs/web.browser/app/new_leaderboard.js
@@ -1,25 +1,6 @@
-(function(){if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+(function(){console.log("Hello World");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+//create a mongo db collection to store player data
+PlayersList = new Mongo.Collection('players');
 
 })();
