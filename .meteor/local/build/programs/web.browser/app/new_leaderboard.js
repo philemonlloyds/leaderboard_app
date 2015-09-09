99a58cc82579504935ee1b8dda8a3950763f4c73
Philemon <philemon@Allens-MacBook-Pro.local>
2015-09-08 20:22:21 -0700

    Add template to html page

--- .meteor/local/build/programs/web.browser/app/new_leaderboard.js
+++ .meteor/local/build/programs/web.browser/app/new_leaderboard.js
@@ -1,4 +1,7 @@
-(function(){console.log("Hello World");
+(function(){if(Meteor.isClient){
+    console.log("Hello World");
+}
+
 
 //create a mongo db collection to store player data
 PlayersList = new Mongo.Collection('players');
--- .meteor/local/build/programs/web.browser/app/template.new_leaderboard.js
+++ .meteor/local/build/programs/web.browser/app/template.new_leaderboard.js
@@ -1,16 +1,14 @@
 (function(){
 Template.body.addContent((function() {
   var view = this;
-  return [ HTML.Raw("<h1>Welcome to Meteor!</h1>\n\n  "), Spacebars.include(view.lookupTemplate("hello")) ];
+  return [ HTML.Raw("<h1>Leaderboard</h1>\n  <!--to make the leaderboard template available in the interface add the following -->\n  <!-- spacebar syntax is used in html to make something appear dynamically -->\n  "), Spacebars.include(view.lookupTemplate("leaderboard")) ];
 }));
 Meteor.startup(Template.body.renderToDocument);
 
-Template.__checkName("hello");
-Template["hello"] = new Template("Template.hello", (function() {
+Template.__checkName("leaderboard");
+Template["leaderboard"] = new Template("Template.leaderboard", (function() {
   var view = this;
-  return [ HTML.Raw("<button>Click Me</button>\n  "), HTML.P("You've pressed the button ", Blaze.View("lookup:counter", function() {
-    return Spacebars.mustache(view.lookup("counter"));
-  }), " times.") ];
+  return "Hello World";
 }));
 
 })();
